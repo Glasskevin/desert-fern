67daefaeac8fa5ba441de2715e5f937d9b43087b
Eva Ogbe <eoogbe@gmail.com>
2016-12-14 20:39:29 -0800

    Fixed controls

--- src/game.js
+++ src/game.js
@@ -6,18 +6,13 @@ export const WIDTH = 500;
 export const HEIGHT = 200;
 const PLAYER_SIZE = 10;
 const WATER_SIZE = 5;
-const GROUND_JUMP_VELOCITY = -6;
-const AIR_JUMP_VELOCITY = -2.5;
+const GROUND_JUMP_VELOCITY = -7;
+const AIR_JUMP_VELOCITY = -5;
 const SPEED_LIMIT = 8;
 const GRAVITY = 0.3;
 const AIR_FRICTION = 1 / 1.25;
 const GROUND_FRICTION = 1 / 1.55;
 const PUSH_X = 5;
-
-const defaultUpdateVx = ({ y, height, vx }) =>
-  vx * (y + height >= HEIGHT ? GROUND_FRICTION : AIR_FRICTION);
-
-const defaultUpdateVy = ({ vy }) => vy + GRAVITY;
 
 const makeWater = (x, y) => ({ x, y, width: WATER_SIZE, height: WATER_SIZE });
 
@@ -29,68 +24,57 @@ const initialWorld = {
     height: PLAYER_SIZE,
     vx: 0,
     vy: 0,
-    updateVx: defaultUpdateVx,
-    updateVy: defaultUpdateVy,
+    ax: 0,
+    ay: GRAVITY,
   },
   waters: [makeWater(0, 0), makeWater(0, HEIGHT - WATER_SIZE), makeWater(WIDTH - 10, 0)],
+};
+
+const isGrounded = ({ y, height }) => y + height >= HEIGHT;
+
+const applyFriction = (player) => player.vx * (isGrounded(player) ? GROUND_FRICTION : AIR_FRICTION);
+
+const updatePlayer = (player) => {
+  const vx = Math.min(SPEED_LIMIT, Math.max(-SPEED_LIMIT, applyFriction(player) + player.ax));
+  const vy = (isGrounded(player) ? Math.min(0, player.vy) : player.vy) + player.ay;
+
+  return {
+    ...player,
+    x: Math.min(WIDTH, Math.max(0, player.x + vx)),
+    y: Math.min(HEIGHT - player.height, player.y + vy),
+    vx,
+    vy,
+  };
+};
+
+const jumpVy = (player) => {
+  if (isGrounded(player)) return GROUND_JUMP_VELOCITY;
+  if (player.y > player.height) return AIR_JUMP_VELOCITY;
+  return player.vy;
 };
 
 const jump = () => (world) => {
   const { player } = world;
 
-  return {
-    ...world,
-    player: {
-      ...player,
-      updateVy({ y, vy }) {
-        if (y + PLAYER_SIZE >= HEIGHT) return GROUND_JUMP_VELOCITY;
-        if (y > PLAYER_SIZE) return AIR_JUMP_VELOCITY;
-        return vy;
-      },
-    },
-  };
-};
-
-const stopVy = () => (world) => {
-  const { player } = world;
-
-  return { ...world, player: { ...player, updateVy: defaultUpdateVy }};
+  return { ...world, player: updatePlayer({ ...player, vy: jumpVy(player) }) };
 };
 
 const backward = () => (world) => {
   const { player } = world;
 
-  return {
-    ...world,
-    player: {
-      ...player,
-      updateVx(player) {
-        const { vx } = player;
-        return defaultUpdateVx({ ...player, vx: Math.max(-SPEED_LIMIT, player.vx - PUSH_X) });
-      },
-    },
-  };
+  return { ...world, player: updatePlayer({ ...player, ax: -PUSH_X }) };
 };
 
 const forward = () => (world) => {
   const { player } = world;
 
-  return {
-    ...world,
-    player: {
-      ...player,
-      updateVx(player) {
-        const { vx } = player;
-        return defaultUpdateVx({ ...player, vx: Math.min(SPEED_LIMIT, player.vx + PUSH_X) });
-      },
-    },
-  };
+  return { ...world, player: updatePlayer({ ...player, ax: PUSH_X }) };
 };
 
 const stopVx = () => (world) => {
   const { player } = world;
 
-  return { ...world, player: { ...player, updateVx: defaultUpdateVx }};
+  return { ...world, player: updatePlayer({ ...player, ax: 0 }) };
 };
 
 const tick = () => (world) => {
@@ -98,13 +82,7 @@ const tick = () => (world) => {
 
   return {
      ...world,
-    player: {
-      ...player,
-      x: Math.min(WIDTH, Math.max(0, player.x + player.vx)),
-      y: Math.min(HEIGHT - player.height, player.y + player.vy),
-      vx: player.updateVx(player),
-      vy: player.updateVy(player),
-    },
+    player: updatePlayer(player),
     waters: waters.filter((water) => !collide(player, water)),
   };
 };
@@ -130,15 +108,13 @@ export default ({ animation, key }) => {
     .map(([a, b]) => a + b)
     .filter((n) => n <= 0);
   const stopVxInput$ = numVxInput$.mapTo(releaseVxInput$).flatten();
-  const stopVyInput$ = key.up('w');
 
   const action$ = xs.merge(
     animation.map(tick),
     jumpInput$.map(jump),
     backwardInput$.map(backward),
     forwardInput$.map(forward),
-    stopVxInput$.map(stopVx),
-    stopVyInput$.map(stopVy)
+    stopVxInput$.map(stopVx)
   );
 
   return {
